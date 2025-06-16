18620d468e32f95ac3c2413ee139cc8f7108c3a7
Nimbraj <nimbarajjagtap@gmail.com>
2025-06-16 18:25:58 +0530

    Created a new folder and added first.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import './App.css'
-import Header from './Component/header'
+import First from './Home/First'
 // Make sure Header.jsx exists in the same directory or adjust the path
 
 function App() {
   return (
     <>
-      <Header/>
-      <div className="p-6">
-        <h1 className="text-2xl font-bold text-gray-800">Welcome to the Home Page</h1>
-        {/* Add more content here */}
-      </div>
+      
+<First/>
+    
     </>
   )
 }
---
+++ src/Home/First.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../Component/header'
+
+export default function First() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
